4b7b2f196ea3eac2eb445e84f43cbe5272a8cb29
prajwalthinking <159680880+prajwalthinking@users.noreply.github.com>
2024-04-05 20:39:27 +0530

    Update login.jsx

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -47,23 +47,23 @@ export function LoginForm() {
     }
   };
 
-  return (
-    <form onSubmit={handleSubmit} className="flex max-w-md flex-col gap-4 p-4 shadow-md shadow-black rounded">
-      <h1 className="text-xl text-center p-2">Login Here</h1>
+   return (
+    <form className="flex max-w-md flex-col gap-4 p-4 shadow-md shadow-black rounded">
+	  <h1 className="text-xl text-center p-2">Login Here</h1>
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="email" value="Your email" />
+          <Label htmlFor="email1" value="Your email" />
         </div>
-        <TextInput id="email" type="email" placeholder="name@flowbite.com" required value={formData.email} onChange={handleChange} />
+        <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
       </div>
       <div>
         <div className="mb-2 block">
-          <Label htmlFor="password" value="Your password" />
+          <Label htmlFor="password1" value="Your password" />
         </div>
-        <TextInput id="password" type="password" required value={formData.password} onChange={handleChange} />
+        <TextInput id="password1" type="password" required />
       </div>
       <div className="flex items-center gap-2">
-        <Checkbox id="remember" checked={formData.rememberMe} onChange={handleChange} />
+        <Checkbox id="remember" />
         <Label htmlFor="remember">Remember me</Label>
       </div>
       <Button type="submit">Submit</Button>
