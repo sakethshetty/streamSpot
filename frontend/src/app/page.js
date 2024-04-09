323d5640ab5b370b5a8b97a60057096b3cd9c5da
prajwalthinking <159680880+prajwalthinking@users.noreply.github.com>
2024-04-09 12:11:06 +0530

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,14 @@
 
 import { LoginForm } from "@/components/login";
+import NavbarComponent from "@/components/navbar";
+import VideoList from "@/components/videos";
 
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex justify-center min-h-screen">
-	  
+	  <NavbarComponent />
 	  <div className="flex flex-col justify-center min-h-screen ">
       <LoginForm />
 	  
