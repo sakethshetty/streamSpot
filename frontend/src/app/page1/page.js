c964be82e3f7626b86476566f227939ce7d5f509
prajwalthinking <159680880+prajwalthinking@users.noreply.github.com>
2024-04-26 08:51:39 +0530

    Add files via upload

---
+++ frontend/src/app/page1/page.js
@@ -0,0 +1,17 @@
+
+import { LoginForm } from "@/components/login";
+
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center min-h-screen">
+	  
+	  <div className="flex flex-col justify-center min-h-screen ">
+      <LoginForm />
+	  
+	  </div>
+    </main>
+  );
+}
