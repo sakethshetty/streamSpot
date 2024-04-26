c32295704acc6eb77daab67a43a6e3d13659731f
prajwalthinking <159680880+prajwalthinking@users.noreply.github.com>
2024-04-26 09:19:11 +0530

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,19 +1,48 @@
-
-import { LoginForm } from "@/components/login";
+"use client";
 import NavbarComponent from "@/components/navbar";
-import VideoList from "@/components/videos";
+import VideoPlayer from "@/components/videoPlayer";
+import VideosSideList from "@/components/videosSideList";
+import { useSearchParams } from "next/navigation";
+import { LoginForm } from "@/components/login";
+
 import { SignupForm } from "@/components/signup";
-
 import Image from "next/image";
+import { Account } from "@/components/account";
+export default function Player() {
+  // get video url from query string
 
-export default function Home() {
+  const videos_data = [
+    {
+      id: 1,
+      title: "Video 1",
+      url: "https://www.youtube.com/watch?v=1",
+    },
+    {
+      id: 2,
+      title: "Video 2",
+      url: "https://www.youtube.com/watch?v=2",
+    },
+    {
+      id: 3,
+      title: "Video 3",
+      url: "https://www.youtube.com/watch?v=3",
+    },
+    {
+      id: 4,
+      title: "Video 4",
+      url: "https://www.youtube.com/watch?v=4",
+    },
+  ];
+  const params = useSearchParams();
+  const videoUrl = params.get("videoUrl");
+  console.log(videoUrl);
   return (
-    <main className="flex justify-center min-h-screen">
-	  <NavbarComponent />
-	  <div className="flex flex-col justify-center min-h-screen ">
-      <LoginForm />
-	  
-	  </div>
-    </main>
+    <>
+      <NavbarComponent />
+      <div className="w-[90%] m-auto md:flex justify-between">
+        <VideoPlayer videoUrl={videoUrl} />
+        <VideosSideList videos={videos_data} />
+      </div>
+    </>
   );
 }
