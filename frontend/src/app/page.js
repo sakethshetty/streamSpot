8ea72523a23e31603816e1271ebcc1b01fc0556c
prajwalthinking <159680880+prajwalthinking@users.noreply.github.com>
2024-04-30 10:00:59 +0530

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,48 +1,46 @@
-"use client";
 import NavbarComponent from "@/components/navbar";
-import VideoPlayer from "@/components/videoPlayer";
-import VideosSideList from "@/components/videosSideList";
-import { useSearchParams } from "next/navigation";
-import { LoginForm } from "@/components/login";
 
-import { SignupForm } from "@/components/signup";
-import Image from "next/image";
-import { Account } from "@/components/account";
-export default function Player() {
-  // get video url from query string
-
-  const videos_data = [
-    {
-      id: 1,
-      title: "Video 1",
-      url: "https://www.youtube.com/watch?v=1",
-    },
-    {
-      id: 2,
-      title: "Video 2",
-      url: "https://www.youtube.com/watch?v=2",
-    },
-    {
-      id: 3,
-      title: "Video 3",
-      url: "https://www.youtube.com/watch?v=3",
-    },
-    {
-      id: 4,
-      title: "Video 4",
-      url: "https://www.youtube.com/watch?v=4",
-    },
-  ];
-  const params = useSearchParams();
-  const videoUrl = params.get("videoUrl");
-  console.log(videoUrl);
+export default function Home() {
+const videos_data = [
+  {
+    id: 1,
+    title: "Video 1",
+    url: "https://www.youtube.com/watch?v=1",
+  },
+  {
+    id: 2,
+    title: "Video 2",
+    url: "https://www.youtube.com/watch?v=2",
+  },
+  {
+    id: 3,
+    title: "Video 3",
+    url: "https://www.youtube.com/watch?v=3",
+  },
+  {
+    id: 4,
+    title: "Video 4",
+    url: "https://www.youtube.com/watch?v=4",
+  },
+];
   return (
-    <>
+    <main className="min-h-screen">
       <NavbarComponent />
-      <div className="w-[90%] m-auto md:flex justify-between">
-        <VideoPlayer videoUrl={videoUrl} />
-        <VideosSideList videos={videos_data} />
-      </div>
-    </>
+		<h1 className="text-3xl font-bold">Videos</h1>
+	  <div className="justify-evenly md:flex  w-[70%] m-auto text-center">
+			{videos_data.map((video) => (
+			  <div key={video.id} className="bg-gray-200 p-4 m-4 sm:flex justify-between md:block">
+				<img src="https://via.placeholder.com/150" alt="video" className="m-auto"/>
+				<div>
+				<h2 className="text-xl font-bold">{video.title}</h2>
+				<a href={`/player?videoUrl=${video.url}`} target="_blank" className="text-blue-500">
+				  Watch Video
+				</a>
+				</div>
+			  </div>
+			))}
+	  </div>
+
+    </main>
   );
 }
